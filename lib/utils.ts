c51af616d3ea7ac86632d65103c73445f570eac0
Kyle <lejeunekyle@gmail.com>
2026-04-04 00:39:45 -0500

    Added cart page and table components, and updated utils with a function to calculate total price from cart items.

--- lib/utils.ts
+++ lib/utils.ts
@@ -52,3 +52,21 @@ export function round2(value: number | string) {
   }
 
 }
+
+const CURRENCY_FORMATTER = new Intl.NumberFormat('en-US', {
+  style: 'currency',
+  currency: 'USD',
+  minimumFractionDigits: 2,
+});
+
+// Format currency using a formatter above
+
+export function formatCurrency(amount: number | string | null) {
+  if (typeof amount === 'number') {
+    return CURRENCY_FORMATTER.format(amount);
+  } else if (typeof amount === 'string') {
+    return CURRENCY_FORMATTER.format(Number(amount));
+  } else {
+    return 'NaN';
+  }
+}
