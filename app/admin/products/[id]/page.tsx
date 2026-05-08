cab542ba8bb1d6f6ea6e04e8a1e06d18bc3ee423
Kyle <lejeunekyle@gmail.com>
2026-05-07 19:03:14 -0500

    ..

--- app/admin/products/[id]/page.tsx
+++ app/admin/products/[id]/page.tsx
@@ -1,6 +1,7 @@
 import ProductForm from "@/components/admin/product-form";
 import { getProductById } from "@/lib/actions/product.actions";
 import { Metadata } from "next";
+import { notFound } from "next/navigation";
 
 export const metadata: Metadata = {
   title: "Update Product",
